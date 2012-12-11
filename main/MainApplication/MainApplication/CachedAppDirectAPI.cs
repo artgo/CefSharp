f658c1b93e04d9e5dbc75b4bff617d1bbeaf62fa
tina.jensen <tina.jensen@appdirec.com>
2012-12-11 11:45:06 -0800

    Create interfaces for AppDirect API

---
+++ main/MainApplication/MainApplication/CachedAppDirectAPI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace MainApplication
+{
+    public class CachedAppDirectAPI
+    {
+        public IEnumerable<Application> MyApps { get; set; }
+        public IEnumerable<Application> SuggestedApps { get; set; }
+    }
+}
