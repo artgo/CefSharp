3e9fd5d7a60abcf6476102fb622fdfbf876e0a1d
Artem GOlubev <artem.golubev@appdirect.com>
2012-12-11 21:53:25 -0800

    Initial commit, adding simple webservice client, unit test project and unit test.

---
+++ main/MainApplication/MainApplication/AppDirectAPI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Script.Serialization;
+using com.appdirect.WindowsClient;
+
+namespace MainApplication
+{
+    public class AppDirectApi
+    {
+        private static readonly AppDirectApi instance = new AppDirectApi();
+        private static readonly Uri ServiceUri = new Uri("https://load.appdirect.com/api/marketplace/v1/listing?filter=FEATURED");
+        private JavaScriptSerializer serializer = new JavaScriptSerializer();
+        private WebClient serviceRequest = new WebClient();
+
+        private AppDirectApi() { }
+
+        public static AppDirectApi Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public applicationsApplication[] MyApps
+        {
+            get { 
+                string result = serviceRequest.DownloadString(ServiceUri);
+                return serializer.Deserialize<applicationsApplication[]>(result);
+            }
+        }
+        public applications SuggestedApps
+        {
+            get
+            {
+                string result = serviceRequest.DownloadString(ServiceUri);
+                return serializer.Deserialize<applications>(result);
+            }
+        }
+    }
+}
---
+++ main/MainApplication/MainApplicationUnitTests/AppDirectApiUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MainApplication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using com.appdirect.WindowsClient;
+
+namespace MainApplicationUnitTests
+{
+    [TestClass]
+    public class AppDirectApiUnitTest
+    {
+        [TestMethod]
+        public void TestDataIsReturned()
+        {
+            applicationsApplication[] apps = AppDirectApi.Instance.MyApps;
+
+            Assert.IsNotNull(apps);
+        }
+    }
+}
