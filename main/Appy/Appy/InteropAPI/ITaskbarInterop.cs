105e9e08ca8135d38094393d326e7f164bf5b1d7
Artem Golubev <artem.golubev@appdirect.com>
2013-02-12 19:48:20 -0800

    Initial button window add

---
+++ main/Appy/Appy/InteropAPI/ITaskbarInterop.cs
@@ -0,0 +1,23 @@
+﻿namespace AppDirect.WindowsClient.InteropAPI
+{
+    public interface ITaskbarInterop
+    {
+        /// <summary>
+        /// Should be called when user changes height of taskbar or choose small icons
+        /// </summary>
+        /// <param name="newHeight"></param>
+        void HeightChanged(int newHeight);
+
+        /// <summary>
+        /// Should be called when user changes newPosition of taskbar on the screen
+        /// </summary>
+        /// <param name="newPosition"></param>
+        void PositionChanged(TaskbarPosition newPosition);
+
+        /// <summary>
+        /// Sets interface for callback events (we want to expand deskband)
+        /// Must be set right away on to support callback events
+        /// </summary>
+        ITaskbarInteropCallback TaskbarCallbackEvents { get; set; }
+    }
+}
---
+++ main/Appy/Appy/InteropAPI/TaskbarAPI.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Controls;
+
+namespace AppDirect.WindowsClient.InteropAPI
+{
+    public class TaskbarApi
+    {
+        private static readonly object SyncObject = new object();
+        private static volatile TaskbarApi _instance = null;
+
+        private TaskbarApi()
+        {
+        }
+
+        public static TaskbarApi Instance
+        {
+            get
+            {
+                lock (SyncObject)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new TaskbarApi();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public void SetTaskbarWindow(Control control, ITaskbarInterop interopObject)
+        {
+            // MUST SET interopObject.TaskbarCallbackEvents = ...
+        }
+
+        public void RemoveTaskbarWindow()
+        {
+
+        }
+    }
+}
