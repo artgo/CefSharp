d4696f4662697b259e0657641e2f6974367a1c55
Artem Golubev <artem.golubev@appdirect.com>
2013-03-07 20:34:15 -0800

    Fixed DC-287. Added some unit tests and made taskbar dragging more responsive

---
+++ main/Appy/Appy.Tests/InteropApi/Internal/CoordsPackagerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AppDirect.WindowsClient.InteropAPI.Internal
+{
+    [TestFixture]
+    public class CoordsPackagerTest
+    {
+        [Test]
+        public void TestPackParamsThrowsOnNullInput()
+        {
+            var packager = new CoordsPackager();
+            Assert.Throws<ArgumentNullException>(delegate { packager.PackParams(null); });
+        }
+
+        [Test]
+        public void TestUnpackParamsAreTheSameAsOriginal()
+        {
+            var packager = new CoordsPackager();
+            var param = new RectWin() { Left = 1, Top = 2, Right = 3, Bottom = 4 };
+            var packedParams = packager.PackParams(param);
+            var result = packager.UnpackParams(packedParams.LParam, packedParams.WParam);
+            Assert.AreEqual(param, result);
+        }
+    }
+}
