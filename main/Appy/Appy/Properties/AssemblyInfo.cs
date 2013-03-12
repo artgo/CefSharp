4820265d90273184b69c9efd78a0381181ea3328
Artem Golubev <artem.golubev@appdirect.com>
2013-03-11 17:56:16 -0700

    Kick up version

--- main/Appy/Appy/Properties/AssemblyInfo.cs
+++ main/Appy/Appy/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.30306.110")]
-[assembly: AssemblyFileVersion("0.0.30306.110")]
+[assembly: AssemblyVersion("0.0.30313.111")]
+[assembly: AssemblyFileVersion("0.0.30313.111")]
