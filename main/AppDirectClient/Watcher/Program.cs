ad33f1c06af8824bd89c7cfa2f433d5cab1ae588
tina.jensen <tina.jensen@appdirec.com>
2013-05-09 14:19:40 -0700

    Added watchdog project

---
+++ main/AppDirectClient/Watcher/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Watcher
+{
+    public class Program
+    {
+        static int count = 3;
+
+        static void Main()
+        {
+            Launch();
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        static void Launch()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "AppDirectClient.exe";
+            process.EnableRaisingEvents = true;
+            process.Exited += LaunchIfCrashed;
+
+            process.Start();
+        }
+
+        static void LaunchIfCrashed(object o, EventArgs e)
+        {
+            Process process = (Process)o;
+            if (process.ExitCode != 0)
+            {
+                if (count-- > 0) // restart at max count times
+                    Launch();
+                else
+                    Environment.Exit(process.ExitCode);
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
