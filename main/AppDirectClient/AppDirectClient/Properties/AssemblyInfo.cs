5a98a321420c3eb5602e099e628b58809c905c84
tina.jensen <tina.jensen@appdirec.com>
2013-05-15 17:57:43 -0700

    updated version, moved to production

--- main/AppDirectClient/AppDirectClient/Properties/AssemblyInfo.cs
+++ main/AppDirectClient/AppDirectClient/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.30501.136")]
-[assembly: AssemblyFileVersion("0.0.30501.136")]
+[assembly: AssemblyVersion("0.0.30515.137")]
+[assembly: AssemblyFileVersion("0.0.30515.137")]
