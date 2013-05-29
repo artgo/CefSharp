1b8b2dacd9cba296ee0cc5f318b8b56cef448dec
Anton Khodakivskiy <akhodakivskiy@gmail.com>
2013-05-29 13:40:27 -0400

    extracted class

--- main/AppDirectClient/AppDirectClient/InteropAPI/Internal/CoordsPackager.cs
+++ main/AppDirectClient/AppDirectClient/InteropAPI/Internal/CoordsPackager.cs
@@ -4,12 +4,6 @@ namespace AppDirect.WindowsClient.InteropAPI.Internal
 {
     public class CoordsPackager
     {
-        public class MessageParams
-        {
-            public IntPtr LParam { get; set; }
-            public IntPtr WParam { get; set; }
-        }
-
         private const int BitsToShift = 16;
         private const int BitMask = 0xFFFF;
 
---
+++ main/AppDirectClient/AppDirectClient/InteropAPI/Internal/MessageParams.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AppDirect.WindowsClient.InteropAPI.Internal
+{
+    public class MessageParams
+    {
+        public IntPtr LParam { get; set; }
+        public IntPtr WParam { get; set; }
+    }
+}
