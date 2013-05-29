2e455f44e2f887fd4907346a23e11b4660224784
tina.jensen <tina.jensen@appdirec.com>
2013-05-28 18:37:17 -0700

    Unit tests!

---
+++ main/AppDirectClient/UnitTests/Common/UI/ApplicationViewModelTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using AppDirect.WindowsClient.UI;
+using NUnit.Framework;
+using Application = AppDirect.WindowsClient.Common.API.Application;
+
+namespace AppDirect.WindowsClient.Tests.Common.UI
+{
+    [TestFixture]
+    public class ApplicationViewModelTests
+    {
+        [Test]
+        public void DefaultHasVisibleContextMenu()
+        {
+            var application = new Application();
+            var avm = new ApplicationViewModel(application);
+
+            Assert.AreEqual(Visibility.Visible, avm.DisplayContextMenu);
+        }
+
+        [Test]
+        public void HideContextMenuHasHiddenVisibility()
+        {
+            var application = new Application() {HideContextMenu = true};
+            var avm = new ApplicationViewModel(application);
+
+            Assert.AreEqual(Visibility.Hidden, avm.DisplayContextMenu);
+        }
+    }
+}
