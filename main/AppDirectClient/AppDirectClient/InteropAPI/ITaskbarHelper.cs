cbbc0e495e7a418b7b11320a17233baae0e410ca
tina.jensen <tina.jensen@appdirec.com>
2013-05-31 15:04:53 -0700

    cleanup

--- main/AppDirectClient/AppDirectClient/InteropAPI/ITaskbarHelper.cs
+++ main/AppDirectClient/AppDirectClient/InteropAPI/ITaskbarHelper.cs
@@ -1,6 +1,9 @@
 ﻿using AppDirect.WindowsClient.InteropAPI.Internal;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
+using System.Text;
 using System.Windows.Forms;
 
 namespace AppDirect.WindowsClient.InteropAPI
@@ -8,33 +11,21 @@ namespace AppDirect.WindowsClient.InteropAPI
     public interface ITaskbarHelper
     {
         bool IsVistaOrUp { get; }
-
         bool IsWin7OrUp { get; }
-
         bool IsWin8OrUp { get; }
 
         Screen TaskbarScreen { get; }
-
         IntPtr TaskBarHwnd { get; }
-
         IntPtr ReBarHwnd { get; }
-
         Rectangle TaskBarRect { get; }
-
         Rectangle ReBarRect { get; }
-
         TaskbarPosition TaskBarPosition { get; }
-
         TaskbarIconsSize TaskBarIconsSize { get; }
-
         double DpiScalingFactor { get; }
 
         Rectangle GetWindowRectangle(IntPtr hwnd);
-
         Rectangle RectWinToRectangle(RECT rectWin);
-
         Rectangle ScreenToClient(IntPtr hWnd, Rectangle screenRect);
-
         Rectangle ClientToScreen(IntPtr hWnd, Rectangle clientRect);
     }
 }
